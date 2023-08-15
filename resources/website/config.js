e632d4290ec954d36cb1971d0784c5bb7d67eadb
EC2 Default User <ec2-user@ip-10-16-10-116.ec2.internal>
2023-08-15 18:41:44 +0000

    added cognito hosted UI url

--- resources/website/config.js
+++ resources/website/config.js
@@ -1,4 +1,4 @@
 window.COFFEE_CONFIG = {
 	API_GW_BASE_URL_STR: "https://zqiv8mnehi.execute-api.us-east-1.amazonaws.com/prod",
-	COGNITO_LOGIN_BASE_URL_STR: null
+	COGNITO_LOGIN_BASE_URL_STR: "https://whbb98cafelab.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=6ir9ee5oge7ub2lvpqcpd02kq5&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fdwtkhlidy5oyw.cloudfront.net%2Fcallback.html"
 };
